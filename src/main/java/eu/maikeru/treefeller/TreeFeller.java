8909378dd5facdbd94abe79cd6c5724b9bfcfa7b
mecha <michenbox@protonmail.com>
2023-06-22 14:14:22 +0100

    Updating the plugin to work + adding the basic code to make the plugin function as intended. Note: Acacia trees do not work.

--- src/main/java/eu/maikeru/treefeller/TreeFeller.java
+++ src/main/java/eu/maikeru/treefeller/TreeFeller.java
@@ -1,5 +1,7 @@
 package eu.maikeru.treefeller;
 
+import eu.maikeru.treefeller.Listeners.breakListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TreeFeller extends JavaPlugin {
@@ -7,7 +9,8 @@ public final class TreeFeller extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Bukkit.getLogger().info("Starting TreeFeller v1.0 SNAPSHOT");
+        getServer().getPluginManager().registerEvents(new breakListener(), this);
     }
 
     @Override
